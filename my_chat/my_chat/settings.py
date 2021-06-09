c260bf75d9ef2f1436d0ccdf5dfdcdc0b2a324c0
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-06-09 14:21:54 +0300

    declaring templates file

--- my_chat/my_chat/settings.py
+++ my_chat/my_chat/settings.py
@@ -36,7 +36,7 @@ ROOT_URLCONF = 'my_chat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
