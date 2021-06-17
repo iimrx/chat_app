0ae276a97778321c817e9ee4f6248e4ece7b7d88
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-06-17 13:42:01 +0300

    new test for the time and date

--- chat/models.py
+++ chat/models.py
@@ -6,6 +6,6 @@ class Room(models.Model):
   name = models.CharField(max_length=500)
 class Message(models.Model):
   text = models.CharField(max_length=5000)
-  date = models.DateTimeField(default=datetime.time, blank=True)
+  date = models.DateTimeField(default=datetime.now(), blank=True)
   user = models.CharField(max_length=500)
   room = models.CharField(max_length=500)
